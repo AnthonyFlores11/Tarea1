33f9ee075d5b9a96d26e9132a54e6e8dc2c3c8ad
ANTHONY FLORES AGUERO <anthony.flores1@uamcr.net>
2020-01-29 15:48:04 -0600

    Tarea

---
+++ Tarea1/Clases/Reproductores.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea1
+{
+    class Reproductores
+    {
+        public void Play()
+        {
+            Console.WriteLine("Play ---------> Inicia la pelicula");
+
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stop ---------> Se detiene la pelicula");
+
+        }
+        public void FW()
+        {
+            Console.WriteLine("FW -----------> Se adelanta la pelicula");
+        } 
+
+        public void RW()
+        {
+            Console.WriteLine("RW -----------> Se devuelve la pelicula");
+        }
+    }
+}
